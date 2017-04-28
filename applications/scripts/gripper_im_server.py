7f8f0215e7bf05a560f83142746bd606643110d8
Kousuke Ariga <koar8470@uw.edu>
2017-04-28 13:07:49 -0700

    Able to move controler

--- applications/scripts/gripper_im_server.py
+++ applications/scripts/gripper_im_server.py
@@ -5,6 +5,7 @@ from interactive_markers.interactive_marker_server import *
 from geometry_msgs.msg import *
 from visualization_msgs.msg import *
 import fetch_api
+from std_msgs.msg import *
 
 GRIPPER_MESH = 'package://fetch_description/meshes/gripper_link.dae'
 L_FINGER_MESH = 'package://fetch_description/meshes/l_gripper_finger_link.STL'
@@ -18,11 +19,11 @@ class GripperTeleop(object):
         self.arm = arm
 
         # InteractiveMarker - top level object
-        gripper_im = InteractiveMarker()
-        gripper_im.header.frame_id = 'base_link'
-        gripper_im.name = GRIPPER_MARKER_NAME
-        gripper_im.description = GRIPPER_MARKER_NAME
-        gripper_im.scale = .25
+        self.gripper_im = InteractiveMarker()
+        self.gripper_im.header.frame_id = 'base_link'
+        self.gripper_im.name = GRIPPER_MARKER_NAME
+        self.gripper_im.description = GRIPPER_MARKER_NAME
+        self.gripper_im.scale = .25
         
         # Initial IM Pose
         pose = Pose()
@@ -30,7 +31,7 @@ class GripperTeleop(object):
         pose.position.y = 0
         pose.position.z = 1
         pose.orientation.w = 1
-        gripper_im.pose = pose
+        self.gripper_im.pose = pose
 
         # Markers for gripper and fingers
         gripper_marker = Marker()
@@ -68,6 +69,8 @@ class GripperTeleop(object):
         r_finger_m.color.r = 1.0
         r_finger_m.color.a = 1.0
 
+        self.mesh = [gripper_marker, l_finger_m, r_finger_m]
+
         # This control is the dropdown menu
         gripper_control = InteractiveMarkerControl()
         gripper_control.orientation.w = 1
@@ -79,7 +82,7 @@ class GripperTeleop(object):
         gripper_control.markers.append(r_finger_m)
 
         gripper_control.always_visible = True
-        gripper_im.controls.append(gripper_control)
+        self.gripper_im.controls.append(gripper_control)
 
         # Controls
 
@@ -89,7 +92,7 @@ class GripperTeleop(object):
         x_axis_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
 
         x_axis_control.always_visible = True
-        gripper_im.controls.append(x_axis_control)
+        self.gripper_im.controls.append(x_axis_control)
 
         y_axis_control = InteractiveMarkerControl()
         y_axis_control.orientation.w = 1
@@ -97,7 +100,7 @@ class GripperTeleop(object):
         y_axis_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
 
         y_axis_control.always_visible = True
-        gripper_im.controls.append(y_axis_control)
+        self.gripper_im.controls.append(y_axis_control)
 
         z_axis_control = InteractiveMarkerControl()
         z_axis_control.orientation.w = 1
@@ -105,7 +108,7 @@ class GripperTeleop(object):
         z_axis_control.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
 
         z_axis_control.always_visible = True
-        gripper_im.controls.append(z_axis_control)
+        self.gripper_im.controls.append(z_axis_control)
 
         # Rotation Controls
         z_axis_rotate = InteractiveMarkerControl()
@@ -114,15 +117,15 @@ class GripperTeleop(object):
         z_axis_rotate.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
 
         z_axis_rotate.always_visible = True
-        gripper_im.controls.append(z_axis_rotate)
-
+        self.gripper_im.controls.append(z_axis_rotate) 
+        
         x_axis_rotate = InteractiveMarkerControl()
         x_axis_rotate.orientation.w = 1
         x_axis_rotate.orientation.z = 1
         x_axis_rotate.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
 
         x_axis_rotate.always_visible = True
-        gripper_im.controls.append(x_axis_rotate)
+        self.gripper_im.controls.append(x_axis_rotate)
 
         y_axis_rotate = InteractiveMarkerControl()
         y_axis_rotate.orientation.w = 1
@@ -130,7 +133,7 @@ class GripperTeleop(object):
         y_axis_rotate.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
 
         y_axis_rotate.always_visible = True
-        gripper_im.controls.append(y_axis_rotate)
+        self.gripper_im.controls.append(y_axis_rotate)
 
         # Menu Entries
         move_menu_entry = MenuEntry()
@@ -145,11 +148,11 @@ class GripperTeleop(object):
         open_menu_entry.id = 3
         open_menu_entry.title = "Open"
 
-        gripper_im.menu_entries.append(move_menu_entry)
-        gripper_im.menu_entries.append(close_menu_entry)
-        gripper_im.menu_entries.append(open_menu_entry)
+        self.gripper_im.menu_entries.append(move_menu_entry)
+        self.gripper_im.menu_entries.append(close_menu_entry)
+        self.gripper_im.menu_entries.append(open_menu_entry)
 
-        self._im_server.insert(gripper_im, feedback_cb = self.handle_feedback)
+        self._im_server.insert(self.gripper_im, feedback_cb = self.handle_feedback)
         
         self._im_server.applyChanges()
 
@@ -158,18 +161,48 @@ class GripperTeleop(object):
         pass
 
     def handle_feedback(self, feedback):
-        id = feedback.menu_entry_id
-        if id == 1:
-            # Move the arm
-            pass
-        elif id == 2:
-            # Close the gripper
-            self.gripper.close()
-        elif id == 3:
-            # Open the gripper
-            self.gripper.open()
-        pass
-
+        if feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
+            id = feedback.menu_entry_id
+            if id == 1:  
+                kwargs = {
+                    'allowed_planning_time': 20,
+                    'execution_timeout': 40,
+                    'num_planning_attempts': 30,
+                    'replan': False,
+                }
+                pose_stamped = PoseStamped()
+                pose_stamped.header.frame_id = "base_link"
+                pose_stamped.pose = feedback.pose
+                                
+                self.arm.move_to_pose(pose_stamped, **kwargs)
+            elif id == 2:
+                # Close the gripper
+                self.gripper.close()
+            elif id == 3:
+                # Open the gripper
+                self.gripper.open()
+        # TODO:Need to update pose locations 
+        if feedback.event_type == InteractiveMarkerFeedback.MOUSE_UP:
+            i_markers = self.gripper_im.controls
+            poseStamped = PoseStamped()
+            poseStamped.header = feedback.header
+            poseStamped.pose = feedback.pose
+            ik = self.arm.compute_ik(poseStamped)
+            
+            color = ColorRGBA()
+            i_marker = self._im_server.get(feedback.marker_name)
+            if ik:
+                color.r = 0.0
+                color.g = 1.0
+                color.a = 1.0
+            else:
+                color.r = 1.0
+                color.g = 0.0
+                color.a = 1.0
+            for marker in i_marker.controls[0].markers:
+                marker.color = color
+            self._im_server.insert(i_marker)
+            self._im_server.applyChanges()
 
 class AutoPickTeleop(object):
     def __init__(self, arm, gripper, im_server):
@@ -189,7 +222,8 @@ def main():
 
     arm = fetch_api.Arm()
     gripper = fetch_api.Gripper()
-    im_server = InteractiveMarkerServer('gripper_im_server')
+    im_server = InteractiveMarkerServer('gripper_im_server', q_size=2)
+    auto_pick_server = InteractiveMarkerServer('auto_pick_im_server', q_size=2)
     teleop = GripperTeleop(arm, gripper, im_server)
     #auto_pick = AutoPickTeleop(arm, gripper, im_server)
     teleop.start()
